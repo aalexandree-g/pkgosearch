7e3276c63c3fa0c3849e34ec91b3770aeb8a02b3
aalexandree-g <aalexandree.g@gmail.com>
2026-03-09 18:48:12 +0100

    Dropdowns for updates and description

--- src/components/rules/rule/Rule.jsx
+++ src/components/rules/rule/Rule.jsx
@@ -1,8 +1,8 @@
-const Rule = ({ title, content }) => {
+const Rule = ({ title, children }) => {
   return (
     <div className="rule">
       <h2 className="rule__title">{title}</h2>
-      <p className="rule__text">{content}</p>
+      <p className="rule__text">{children}</p>
     </div>
   )
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,9 @@
-import { Link } from 'react-router-dom'
+import { useState } from 'react'
 import Header from '../../components/header/Header'
+import Rule from '../../components/rules/rule/Rule'
 import SearchInput from '../../components/search/SearchInput'
 import ResultBox from '../../components/result/ResultBox'
+import { ChevronDown, ChevronUp } from 'lucide-react'
 
 import { useHomeLogic } from '../../hooks/useHomeLogic'
 import { useAutoResizeTextarea } from '../../hooks/useAutoResizeTextarea'
@@ -9,46 +11,81 @@ import { useAutoResizeTextarea } from '../../hooks/useAutoResizeTextarea'
 const Home = () => {
   const { ref, onInput } = useAutoResizeTextarea()
   const homeLogic = useHomeLogic({ onResize: onInput })
+  const [open, setOpen] = useState(false)
+  const [openUpdate, setOpenUpdate] = useState(false)
 
   return (
     <div className="app">
       <Header />
       <form className="home" onSubmit={homeLogic.handleSubmit}>
         <div className="u-surface rules">
-          Update - March 6th<li>added a description of the tool</li>
-          <li>added a character counter for the result</li>
+          <div
+            className="u-surface__header"
+            onClick={() => setOpenUpdate(!openUpdate)}
+          >
+            <div className="u-surface__title">Update - March 6th, 2026</div>
+            <button className="u-surface__button" type="button">
+              {openUpdate ? <ChevronUp /> : <ChevronDown />}
+            </button>
+          </div>
+          {openUpdate && (
+            <div className="u-surface__content">
+              <li>added a description of the tool</li>
+              <li>added a character counter for the result</li>
+            </div>
+          )}
         </div>
         <div className="u-surface rules">
-          <p>
-            <strong>Pokémon GO's search bar is powerful...</strong>
-            <br />
-            ... but writing complex filters can quickly become a headache.
-            <br />
-            <br />
-            <strong>PoGO Search Plus</strong> lets you build your search{' '}
-            <strong>using parentheses</strong> to group conditions naturally,
-            just like you'd think them. Paste your expression, and the tool
-            instantly converts it into a format the game actually understands.
-            <br />
-            <br />
-            <strong>Example</strong>: Want all your shiny Pikachu and all your
-            XXL Pumpkaboo? Just type
-            <br />
-            <code>(pikachu&shiny),(pumpkaboo&xxl)</code>
-            <br />
-            and let the tool handle the rest.
-            <br />
-            <br />
-            Refer{' '}
-            <a
-              href="https://leidwesen.github.io/SearchPhrases/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              this document
-            </a>{' '}
-            for everything to know about in-game searches.
-          </p>
+          <div className="u-surface__header" onClick={() => setOpen(!open)}>
+            <div className="u-surface__title">
+              <strong>What is PoGO Search Plus ?</strong>
+            </div>
+            <button className="u-surface__button" type="button">
+              {open ? <ChevronUp /> : <ChevronDown />}
+            </button>
+          </div>
+          {open && (
+            <p className="u-surface__content">
+              Pokémon GO's search bar is powerful but writing complex filters
+              can quickly become a headache.
+              <br />
+              <br />
+              <Rule title="Group conditions with parentheses">
+                This app lets you build your search{' '}
+                <strong>using parentheses</strong> to group conditions
+                naturally, just like you'd think them. Paste your expression,
+                and the tool instantly converts it into a format the game
+                actually understands.{' '}
+              </Rule>
+              <br />
+              <Rule title="No more duplicate conditions">
+                Beyond conversion, it also cleans up your expressions
+                automatically: duplicate conditions, redundant terms, and
+                unnecessary complexity are removed, so your final search string
+                is always as short and efficient as possible.
+              </Rule>
+              <br />
+              <Rule title="Example">
+                Want all your <strong>shiny Pikachu</strong> and all your{' '}
+                <strong>XXL Pumpkaboo</strong>? Just type
+                <br />
+                <code>(pikachu&shiny),(pumpkaboo&xxl)</code>
+                <br />
+                and let the tool handle the rest.
+              </Rule>
+              <br />
+              <br />
+              For a full list of in-game search terms, check out{' '}
+              <a
+                href="https://leidwesen.github.io/SearchPhrases/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                this document
+              </a>
+              .
+            </p>
+          )}
         </div>
         <SearchInput
           refEl={ref}
